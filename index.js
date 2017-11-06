02004b0632e30cfea208653299643174b8175891
Amy Jackson <aj81623@gmail.com>
2017-11-06 20:32:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,4 @@ let newID = oldID + 1000000000;
 let ageIsValid = Number.isInteger(currentAge);
 let randomNumber = (Math.random(0,20) *10);
 let randomInteger = (Math.floor(randomNumber));
+let randomUserID = (Math.Random(1000000001,1000000020));
